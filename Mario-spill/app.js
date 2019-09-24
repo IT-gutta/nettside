ecc2702b674126c80207a93c59628d176105a4c5
jorgensandhaug <55702375+jorgensandhaug@users.noreply.github.com>
2019-09-24 18:00:32 +0200

    hopppe og bevegelse

--- Mario-spill/app.js
+++ Mario-spill/app.js
@@ -5,9 +5,10 @@ canvas.height= window.innerHeight-20;
 
 
 var controller={
-  right:false,
-  left:false,
-  up:false,
+  right: false,
+  left: false,
+  up: false,
+  jumping: false,
 }
 
 
@@ -26,16 +27,52 @@ function control(event){
   window.addEventListener("keydown", control)
   window.addEventListener("keyup", control)
 
-  function createRect(x, y, width, height){
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
+  function Rect(x, y, width, height){
+    this.x = x,
+    this.y = y,
+    this.width = width,
+    this.height = height,
+    this.dx = 0,
+    this.dy = 0,
+    this.gravity=0.5,
 
     this.draw = function(){
       c.beginPath();
       c.fillStyle= "orange";
-      c.fill(this.x, this.y, this.width, this.height);
+      c.fillRect(this.x, this.y, this.width, this.height);
       c.fill();
+    },
+
+    this.update= function(){
+      if(controller.right){
+        this.dx=6}
+      if(controller.left){
+        this.dx=-6}
+        else if(!controller.left&&!controller.right){
+          this.dx=0}
+
+      if(controller.up && !controller.jumping){
+        this.dy=-20; controller.jumping= true;
+
+      }
+      if(this.y>canvas.height-100){
+        this.y=canvas.height-100; this.dy=0; controller.jumping= false;
+      }
+
+      this.x+=this.dx;
+      this.y+=this.dy;
+      this.dy+=this.gravity;
+      this.draw();
     }
   }
+
+  var kloss = new Rect(200, canvas.height-100, 100, 100)
+
+
+function animate(){
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height)
+  kloss.update();
+
+}
+animate();
