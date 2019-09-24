a71afac1a6eca67b581e0f9fb77ef11660978296
jorgensandhaug <55702375+jorgensandhaug@users.noreply.github.com>
2019-09-24 21:21:02 +0200

    comments
    asd

--- Mario-spill/app.js
+++ Mario-spill/app.js
@@ -3,7 +3,7 @@ var c= canvas.getContext("2d");
 canvas.width= window.innerWidth-20;
 canvas.height= window.innerHeight-20;
 
-
+// controller-objekt som brukes hele tiden
 var controller={
   right: false,
   left: false,
@@ -11,10 +11,12 @@ var controller={
   jumping: false,
 }
 
-
+// sjekker når man trykker på en knapp og når man slipper den. aka når man holder den inne.
 function control(event){
   var keyS = (event.type=="keydown")?true:false;
-  console.log(event.keyCode);
+  //så lenge man holder en knapp inne, er også booleanen til (controller.prototype.button) true.
+
+
   switch (event.keyCode) {
     case 39: controller.right= keyS; break;
     case 68: controller.right= keyS; break;
@@ -24,15 +26,22 @@ function control(event){
     case 87: controller.up= keyS; break;
   }
   }
+
   window.addEventListener("keydown", control)
   window.addEventListener("keyup", control)
+
+
+//funksjon for å create et rektangel
+// hvis man skriver new Rect(parametere), får man et objekt med keys og methods som er lagt inn i funksjonen
 
   function Rect(x, y, width, height){
     this.x = x,
     this.y = y,
     this.width = width,
     this.height = height,
+    //fart i x-retning
     this.dx = 0,
+    //fart i y-retning
     this.dy = 0,
     this.gravity=0.5,
 
@@ -44,6 +53,7 @@ function control(event){
     },
 
     this.update= function(){
+      //alt som skal skje i animate()-funksjonen
       if(controller.right){
         this.dx=6}
       if(controller.left){
@@ -51,10 +61,13 @@ function control(event){
         else if(!controller.left&&!controller.right){
           this.dx=0}
 
+      // kan bare hoppe hvis controlle.jumping er false, altså at man står på bakken
       if(controller.up && !controller.jumping){
         this.dy=-20; controller.jumping= true;
 
       }
+      //med en gang klossen beveger seg under starthøyden, flyttes den til den opprinnelige y-posisjonen
+      // og controller.jumping gjøres til false slik at man kan hoppe på nytt etter dette
       if(this.y>canvas.height-100){
         this.y=canvas.height-100; this.dy=0; controller.jumping= false;
       }
@@ -62,6 +75,8 @@ function control(event){
       this.x+=this.dx;
       this.y+=this.dy;
       this.dy+=this.gravity;
+      //kjører draw()-funksjonen inni update, slik at klossen blir tegnet på nytt med oppdatert plassering
+      // inni animate()-funksjonen. 
       this.draw();
     }
   }
@@ -69,8 +84,10 @@ function control(event){
   var kloss = new Rect(200, canvas.height-100, 100, 100)
 
 
+//kjøres kontunierlig
 function animate(){
   requestAnimationFrame(animate);
+  //sørger for at når klossen beveger seg, fjernes der den stod fra før, og bare den nye posisjonen vises på canvas
   c.clearRect(0, 0, canvas.width, canvas.height)
   kloss.update();
 
