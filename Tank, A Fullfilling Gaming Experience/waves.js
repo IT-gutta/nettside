0ab8bb5a87d638abe45d5930e5716561c453b1e6
jorgensandhaug <jorgensandhaug@users.noreply.github.com>
2020-01-15 15:56:02 +0100

    adad

---
+++ Tank, A Fullfilling Gaming Experience/waves.js
@@ -0,0 +1,155 @@
+function startNewWave(text, countDownTime, text2, foo){
+    let rgbAlpha = 0
+    let tempTime = 0
+    overlayInterval = setInterval(() => {
+        if(!stop){
+            overlay.style.color = `rgba(0, 0, 0, ${rgbAlpha})`
+            overlay.innerHTML = `${text} ${countDownTime - Math.ceil(tempTime)} <br> <br> <p>${text2}</p>`
+            if(rgbAlpha < 1) rgbAlpha += 0.002
+            tempTime += 0.01
+            if(tempTime > countDownTime) foo()
+        }
+    }, 10)
+}
+
+
+let waves = [
+    () => {
+        readyToStartNewWave = false
+        canvas.style.filter = "blur(3px)"
+        startNewWave("Wave 1 starting in", 4, "Move with the WASD keys <br> Aim and shoot with the mouse <br> Press ESC to pause/unpause the game at any time <br> Press H to use Health Potion <br> <br> Try to survive as long as possible",
+        function(){
+            clearInterval(overlayInterval)
+            overlay.innerHTML = ""
+            stop = false
+            canvas.style.filter = "none"
+            let antallHunters = 6                                                       
+            let deployedHunters = 1
+            hunterInterval = setInterval(() => {
+                if(!stop){
+                    if(deployedHunters == antallHunters){
+                        clearInterval(hunterInterval)
+                        readyToStartNewWave = true
+                    }
+                    deployedHunters += 1
+                    hunterArr.push(new Sploder(1, 100))
+                }
+            }, 1000)
+        }
+    )
+},
+    () => {
+        readyToStartNewWave = false
+        canvas.style.filter = "blur(3px)"
+        startNewWave("Wave 2 starting in", 10, "Tankier enemies",
+        function(){
+            clearInterval(overlayInterval)
+            overlay.innerHTML = ""
+            stop = false
+            canvas.style.filter = "none"
+            let antallHunters = 12                                                      
+            let deployedHunters = 1
+            hunterInterval = setInterval(() => {
+                if(!stop){
+                    if(deployedHunters == antallHunters){
+                        clearInterval(hunterInterval)
+                        readyToStartNewWave = true
+                    }
+                    deployedHunters += 1
+                    hunterArr.push(new Hunter(1, 150))
+                }
+            }, 1000)
+        }
+    )
+},
+() => {
+        readyToStartNewWave = false
+        canvas.style.filter = "blur(3px)"
+        startNewWave("Wave 3 starting in", 10, "Increased speed",
+        function(){
+            clearInterval(overlayInterval)
+            overlay.innerHTML = ""
+            stop = false
+            canvas.style.filter = "none"
+            let antallHunters = 24                                                    
+            let deployedHunters = 1
+            hunterInterval = setInterval(() => {
+                if(!stop){
+                    if(deployedHunters == antallHunters){
+                        clearInterval(hunterInterval)
+                        readyToStartNewWave = true
+                    }
+                    deployedHunters += 1
+                    hunterArr.push(new Hunter(2, 150))
+                }
+            }, 1000)
+        }
+    )
+},
+    () => {
+        readyToStartNewWave = false
+        canvas.style.filter = "blur(3px)"
+        startNewWave("Wave 4 starting in", 10, "Quicker spawns",
+        function(){
+            clearInterval(overlayInterval)
+            overlay.innerHTML = ""
+            stop = false
+            canvas.style.filter = "none"
+            let antallHunters = 35                                                      
+            let deployedHunters = 1
+            hunterInterval = setInterval(() => {
+                if(!stop){
+                    if(deployedHunters == antallHunters){
+                        clearInterval(hunterInterval)
+                        readyToStartNewWave = true
+                    }
+                    deployedHunters += 1
+                    hunterArr.push(new Hunter(2, 150))
+                }
+            }, 700)
+        }
+    )
+},
+() => {
+    readyToStartNewWave = false
+    canvas.style.filter = "blur(3px)"
+    startNewWave("Wave 5 starting in", 10, "Even tankier",
+    function(){
+        clearInterval(overlayInterval)
+        overlay.innerHTML = ""
+        stop = false
+        canvas.style.filter = "none"
+        let antallHunters = 40                                                     
+        let deployedHunters = 1
+        hunterInterval = setInterval(() => {
+            if(!stop){
+                if(deployedHunters == antallHunters){
+                    clearInterval(hunterInterval)
+                    readyToStartNewWave = true
+                }
+                deployedHunters += 1
+                hunterArr.push(new Hunter(2, 200))
+            }
+        }, 700)
+    }
+)
+},
+    () => {
+        readyToStartNewWave = false
+        canvas.style.filter = "blur(3px)"
+        startNewWave("Freemode starting in", 10, "Have fun",
+        function(){
+            wave = "Freemode"
+            clearInterval(overlayInterval)
+            overlay.innerHTML = ""
+            stop = false
+            canvas.style.filter = "none"                                                  
+            hunterInterval = setInterval(() => {
+                if(!stop){
+                    hunterArr.push(new Hunter(2, 150))
+                }
+            }, 700)
+        }
+    )
+},
+]
