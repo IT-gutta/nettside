06560607bf19c8708e9d5efa02d51c57aed74ab5
bragem <56249709+bragem@users.noreply.github.com>
2020-04-01 16:18:52 +0200

    sfdse

--- flappyBird/intro.js
+++ flappyBird/intro.js
@@ -1,20 +1,17 @@
 var knappEl = document.getElementById("knapp")
 var inputEl = document.getElementById("navn")
 var p = document.getElementById("p")
+var form = document.querySelector("form")
 
-knappEl.onclick = function(){
+form.onsubmit = function(){
+    location.href = "index.html"
     //om siden ikke finner et lagret navn må du skrive inn navnet ditt, hvis ikke blir du sendt til spillet
     if(window.localStorage.getItem("name") != null){
         location.href = "index.html"
     }
     else{
-        if(inputEl.value == ""){
-            p.innerHTML = "Du må skrive inn navnet ditt for å spille"
-        }
-        else{
             var navn = inputEl.value
             window.localStorage.setItem("name",navn)
             location.href = "index.html"
         }
     }
-}
