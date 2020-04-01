2af278fa35736561f266264b95297d89f626548f
bragem <56249709+bragem@users.noreply.github.com>
2020-04-01 17:51:44 +0200

    ffgdsfgd

--- flappyBird/intro.js
+++ flappyBird/intro.js
@@ -2,16 +2,28 @@ var knappEl = document.getElementById("knapp")
 var inputEl = document.getElementById("navn")
 var p = document.getElementById("p")
 var form = document.querySelector("form")
+var intro = document.getElementById("intro")
 
-form.onsubmit = function(){
-    // location.href = "index.html"
+var container = document.getElementById("container")
+
+function startSpill(){
+     //viser spillet og skjuler innloggingen
+     container.style = "display: flex;"
+     intro.style = "display: none;"
+}
+
+form.onsubmit = function(e){
+    //denne er med for å hindre at siden lastes inn på nytt
+    e.preventDefault()
     //om siden ikke finner et lagret navn må du skrive inn navnet ditt, hvis ikke blir du sendt til spillet
     if(window.localStorage.getItem("name") != null){
-        location.href = "index.html"
+
+       startSpill()
     }
     else{
             var navn = inputEl.value
             window.localStorage.setItem("name",navn)
-            location.href = "index.html"
-        }
+
+           startSpill()
     }
+}
